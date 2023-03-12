c205a8312a5b2e656df7256de859010392f75daa
electimon <electimon@gmail.com>
2023-03-12 03:04:37 +0000

    Spacewar: Rename manifest_yupik.xml to manifest.xml
    * CAF only does this because they use one device tree
      for all 3 SoCs, vendor/qcom/lahaina. We use one -common
      tree with device trees inheriting it. There is no need
      to have manifest_yupik.xml.
    
    Change-Id: I133f19725d523ce2e4acb17114b687f9e5757019
    Signed-off-by: electimon <electimon@gmail.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(CUSTOM_PRODUCT_DIR)/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest_yupik.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
