0d19a2b5762162129faea3daa7bf5abb8beeb8f4
Simão Gomes Viana <devel@superboring.dev>
2024-09-22 00:53:36 +0200

    Use lineage libperfmgr
    It's basically the same thing just newer, so why bother having it in the
    device tree if pretty much every device is using it.

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     vendor/qcom/opensource/usb/etc
 
 # A/B
@@ -412,7 +413,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.nothing-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power@1.3.vendor
 
 PRODUCT_COPY_FILES += \
