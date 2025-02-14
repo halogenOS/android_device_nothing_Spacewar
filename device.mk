a3d89aff2c5e34ba3e35503abb56e6b248a76702
Simão Gomes Viana <devel@superboring.dev>
2025-02-14 21:22:43 +0100

    Remove ParanoidGlyph
    i dont want everything to be crashing all the time so lets remove it for
    now until it works again

--- device.mk
+++ device.mk
@@ -263,10 +263,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
-# Glyph
-PRODUCT_PACKAGES += \
-    ParanoidGlyphPhone1
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
