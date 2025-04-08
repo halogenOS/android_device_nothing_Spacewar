f68a714903c485075fffb50f5ab7350ae4458331
Simão Gomes Viana <devel@superboring.dev>
2025-04-09 00:56:54 +0200

    Add back powershare

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := libudfps_extension.nothing
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    $(CUSTOM_PRODUCT_DIR)/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
